823f3a98d8668331010e8ca9c51df668e66c8093
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-03-28 15:18:22 +0300

    modificated class Main

--- Encapsulation/04_Copy/src/Main.java
+++ Encapsulation/04_Copy/src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Dimensions dimensions = new Dimensions(3, 5, 7);
+        Dimensions dimensions = new Dimensions(3,5,7);
         Dimensions a = dimensions.setWidth(2);
-        Dimensions b = dimensions.setLength(4);
+        Dimensions b = dimensions.setHeight(4);
         Dimensions c = dimensions.setLength(6);
-        Cargo cargo = new Cargo(dimensions, 4.4, "г.Москва, ул.Ленинская, д.17");
+        Cargo cargo = new Cargo(dimensions,4.4,"г.Москва, ул.Ленинская, д.17");
         // some code
-        Cargo a1 = cargo.setDimensions(new Dimensions(2, 4, 6));
-        Cargo b1 = cargo.setWeight(6.4);
+        Cargo a1 = cargo.setDimensions(new Dimensions(2,4,6));
+        Cargo b1 = cargo.setWeight(6.8);
         Cargo c1 = cargo.setDeliveryAddress("г.Минск, ул.Макаенка, д.8");
+        Cargo cargo1 = new Cargo(new Dimensions(2,4,6), 6.8,"г.Минск, ул.Макаенка, д.8");
+        System.out.println(cargo1);
         Dimensions copy = new Dimensions (
                 dimensions.getWidth(),
                 dimensions.getHeight(),
@@ -17,6 +19,5 @@ public class Main {
                 cargo.getDimensions(),
                 cargo.getWeight(),
                 cargo.getDeliveryAddress());
-        System.out.println(new Cargo(new Dimensions(2, 4, 6), 6.4, "г.Минск, ул.Макаенка, д.8"));
     }
 }
