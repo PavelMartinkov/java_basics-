0feb21362dee834abb03f077169b96e7260ec77d
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-08-08 18:05:31 +0300

    task2

--- AdvancedOOPFeatures/homework_2/Employees/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Employees/src/main/java/Main.java
@@ -1,10 +1,19 @@
+import org.apache.commons.collections.iterators.EmptyOrderedIterator;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Stream;
+
 
 public class Main {
 
     private static final String STAFF_TXT = "data/staff.txt";
 
     public static void main(String[] args) {
+
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
         System.out.println(employeeMaxSalary);
@@ -13,6 +22,15 @@ public class Main {
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
         //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
         // кто пришёл в году, указанном в переменной year
-        return null;
+        return staff.stream()
+                .filter(employee -> convertToLocalDate(employee.getWorkStart()).getYear() == year)
+                .max(Comparator.comparing(Employee::getSalary))
+                .get();
+    }
+
+    public static LocalDate convertToLocalDate(Date dateToConvert) {
+        return dateToConvert.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
     }
 }
