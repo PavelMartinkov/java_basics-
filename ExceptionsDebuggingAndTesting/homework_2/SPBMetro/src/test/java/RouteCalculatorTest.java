afd538641425c8cd36022ec680183cd792e7c446
Pavel Martinkov <pavel.martinkov98@mail.ru>
2022-09-04 21:04:14 +0300

    task

--- ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ ExceptionsDebuggingAndTesting/homework_2/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -6,77 +6,85 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RouteCalculatorTest extends TestCase {
 
-    List<Station> route = new ArrayList<>();
-    StationIndex stationIndex = new StationIndex();
-    RouteCalculator routeCalculator = new RouteCalculator(stationIndex);
-    List<Station> oneConnection = new ArrayList<>();
-    List<Station> twoConnection = new ArrayList<>();
+    List<Station> route;
+    StationIndex stationIndex;
+    RouteCalculator routeCalculator;
 
     @Before
     protected void setUp() throws Exception {
+        route = new ArrayList<>();
+        stationIndex = new StationIndex();
+        routeCalculator = new RouteCalculator(stationIndex);
 
-        Line line1 = new Line(1,"Первая");
-        Line line2 = new Line(2,"Вторая");
-        Line line3 = new Line(3,"Третья");
+        List<Line> lines = Arrays.asList(
+            new Line(1,"Первая"),
+            new Line(2,"Вторая"),
+            new Line(3,"Третья"));
 
-        Station station1 = new Station("Петровская", line1);
-        Station station2 = new Station("Арбузная", line1);
-        Station station3 = new Station("Морковная", line2);
-        Station station4 = new Station("Яблочная", line2);
-        Station station5 = new Station("Московская",line3);
-        Station station6 = new Station("Минская",line3);
+        lines.forEach(l -> stationIndex.addLine(l));
 
-        line1.addStation(station1);
-        line1.addStation(station2);
-        line2.addStation(station3);
-        line2.addStation(station4);
-        line3.addStation(station5);
-        line3.addStation(station6);
+        List<Station> stations = Arrays.asList(
+                new Station("Петровская", stationIndex.getLine(1)),
+                new Station("Арбузная", stationIndex.getLine(1)),
+                new Station("Октябрьская", stationIndex.getLine(1)),
+                new Station("Морковная", stationIndex.getLine(2)),
+                new Station("Яблочная", stationIndex.getLine(2)),
+                new Station("Пушкинская", stationIndex.getLine(2)),
+                new Station("Московская",stationIndex.getLine(3)),
+                new Station("Киевская",stationIndex.getLine(3)),
+                new Station("Минская",stationIndex.getLine(3)));
 
-        stationIndex.addStation(station1);
-        stationIndex.addStation(station2);
-        stationIndex.addStation(station3);
-        stationIndex.addStation(station4);
-        stationIndex.addStation(station5);
-        stationIndex.addStation(station6);
-
-        stationIndex.addLine(line1);
-        stationIndex.addLine(line2);
-        stationIndex.addLine(line3);
-
-        route.add(station1);
-        route.add(station2);
-        route.add(station3);
-        route.add(station4);
-        route.add(station5);
-        route.add(station6);
-
-        oneConnection.add(station2);
-        oneConnection.add(station3);
-        stationIndex.addConnection(oneConnection);
-
-        twoConnection.add(station4);
-        twoConnection.add(station5);
-        stationIndex.addConnection(twoConnection);
+        stations.forEach(s -> {
+            s.getLine().addStation(s);
+            stationIndex.addStation(s);
+        });
 
 
+        stationIndex.addConnection(Arrays.asList(
+                stationIndex.getStation("Петровская"),
+                stationIndex.getStation("Пушкинская")));
+        stationIndex.addConnection(Arrays.asList(
+                stationIndex.getStation("Яблочная"),
+                stationIndex.getStation("Московская")));
+        stationIndex.addConnection(Arrays.asList(
+                stationIndex.getStation("Арбузная"),
+                stationIndex.getStation("Минская")));
+
+
+        route.add(stationIndex.getStation("Петровская"));
+        route.add(stationIndex.getStation("Арбузная"));
+        route.add(stationIndex.getStation("Октябрьская"));
+        route.add(stationIndex.getStation("Морковная"));
+        route.add(stationIndex.getStation("Яблочная"));
+        route.add(stationIndex.getStation("Пушкинская"));
+        route.add(stationIndex.getStation("Московская"));
+        route.add(stationIndex.getStation("Киевская"));
+        route.add(stationIndex.getStation("Минская"));
     }
+
     @Test
-    public void testGetShortestRoute() {
-        stationIndex.addConnection(route);
-        List<Station> actual = routeCalculator.getShortestRoute(stationIndex.getStation("Петровская"), stationIndex.getStation("Минская"));
-        List<Station> expected = route.subList(0, route.size());
+    public void testGetShortestRouteWithOneConnection() {
+        List<Station> actual = routeCalculator.getShortestRoute(stationIndex.getStation("Петровская"), stationIndex.getStation("Морковная"));
+        List<Station> expected = List.of(route.get(0),route.get(5), route.get(4), route.get(3));
         assertEquals(expected,actual);
     }
+
+//    @Test
+//    public void testGetShortestRouteWithTwoConnection() {
+//        List<Station> actual = routeCalculator.getShortestRoute(stationIndex.getStation("Петровская"), stationIndex.getStation("Московская"));
+//        List<Station> expected = List.of(route.get(0),route.get(5), route.get(4), route.get(3));
+//        assertEquals(expected,actual);
+//    }
 
         @Test
         public void testCalculateDuration() {
         double actual = RouteCalculator.calculateDuration(route);
-        double expected = 14.5;
+        double expected = 22.0;
         assertEquals(expected,actual);
     }
 
