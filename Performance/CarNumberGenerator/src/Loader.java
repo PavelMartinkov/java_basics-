89bd26269136019e0e100a3a1353dcb809a1a8fe
Pavel Martinkov <pavel.martinkov98@mail.ru>
2023-02-10 18:22:25 +0300

    add thread

--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,41 +1,56 @@
-import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
-public class Loader {
+public class Loader extends Thread {
 
     public static void main(String[] args) throws Exception {
-        long start = System.currentTimeMillis();
+        int cores = Runtime.getRuntime().availableProcessors();
+        System.out.println("Количество ядер: " + cores + System.lineSeparator());
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        for (int i = 1; i <= cores; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        long start = System.currentTimeMillis();
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
+                        char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+                        for (int regionCode = 1; regionCode <= 100; regionCode++) {
+                            PrintWriter writer = new PrintWriter("res/numbers" + regionCode + ".txt");
+                            StringBuilder builder = new StringBuilder();
+                            for (int number = 1; number < 1000; number++) {
+                                for (char firstLetter : letters) {
+                                    for (char secondLetter : letters) {
+                                        for (char thirdLetter : letters) {
+                                            builder.append(firstLetter);
+                                            builder.append(padNumber(number, 3));
+                                            builder.append(secondLetter);
+                                            builder.append(thirdLetter);
+                                            builder.append(padNumber(regionCode, 2));
+                                            builder.append("\n");
+                                        }
+                                    }
+                                }
+                            }
+                            writer.write(builder.substring(0, builder.length()));
+                            writer.flush();
+                            writer.close();
+                        }
+
+                        System.out.println("Parsing time for: " + Thread.currentThread().getName() + " - " + (System.currentTimeMillis() - start) + " ms");
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
-            }
+            });
+            thread.start();
         }
-
-        writer.flush();
-        writer.close();
-
-        System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 
     private static String padNumber(int number, int numberLength) {
         String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
+        while (numberStr.length() < numberLength) {
+            numberStr = "0".concat(numberStr);
         }
-
         return numberStr;
     }
 }
